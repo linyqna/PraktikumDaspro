5b1483a33d75fb207e861b64ba52ab45b199a039
lyniqna <fitriani.widodo18@gmail.com>
2025-10-09 12:30:05 +0700

    Attepmt 2

---
+++ jobsheet5/switchCetakKRS09.java
@@ -0,0 +1,43 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class switchCetakKRS09 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("---Print KRS SIAKAD---");
+        System.out.print("Enter current semester: ");
+        int semester =  inp.nextInt();
+
+        switch (semester) {
+            case 1:
+                System.out.println("KRS Semester 1 is displayed");
+                break;
+            case 2:
+                System.out.println("KRS Semester 2 is displayed");
+                break;
+            case 3:
+                System.out.println("KRS Semester 3 is displayed");
+                break;
+            case 4:
+                System.out.println("KRS Semester 4 is displayed");
+                break;
+            case 5:
+                System.out.println("KRS Semester 5 is displayed");
+                break;
+            case 6:
+                System.out.println("KRS Semester 6 is displayed");
+                break;
+            case 7:
+                System.out.println("KRS Semester 7 is displayed");
+                break;
+            case 8:
+                System.out.println("KRS Semester 8 is displayed");
+                break;
+            default:
+                System.out.println("Invalid Semester");
+        }
+    }
+    
+}

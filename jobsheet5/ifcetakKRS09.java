e8900868afe31212e4eef8b39726947b4ed959e9
lyniqna <fitriani.widodo18@gmail.com>
2025-10-09 12:15:21 +0700

    attemp 1

---
+++ jobsheet5/ifcetakKRS09.java
@@ -0,0 +1,21 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class ifcetakKRS09 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("---Print KRS SIAKAD---");
+        System.out.println("Have the tuition fees been paid in full? (true/false): ");
+        boolean uktLunas = inp.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("UKT payment has been verified");
+            System.out.println("KRS now can be printed and you can ask the academic advisior to sign it.");
+
+            System.out.println("Dilarang mempersulit hidup orang.");
+            
+        }
+    }
+}

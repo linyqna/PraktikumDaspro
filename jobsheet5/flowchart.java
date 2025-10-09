6822cc5398c17d0e1b5e662fae87acf697a2664a
lyniqna <fitriani.widodo18@gmail.com>
2025-10-09 16:15:18 +0700

    Task 2

---
+++ jobsheet5/flowchart.java
@@ -0,0 +1,19 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class flowchart {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("===Even/Odd Number Checker===");
+        System.out.println("enter the number: ");
+        int angka = inp.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else{
+            System.out.println("Ganjil");
+        }
+    }
+}

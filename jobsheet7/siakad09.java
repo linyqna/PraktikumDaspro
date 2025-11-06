c2a53287baf6f4eb21241815635d4d55f52d49a9
lyniqna <fitriani.widodo18@gmail.com>
2025-11-06 11:22:39 +0700

    first

---
+++ jobsheet7/siakad09.java
@@ -0,0 +1,18 @@
+package jobsheet7;
+
+import java.util.Scanner;
+
+public class siakad09 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        double grade;
+        double highest = 0;
+        double lowest = 100;
+        
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Input student's grade: " + i);
+        }
+        
+    }
+}

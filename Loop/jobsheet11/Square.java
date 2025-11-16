f8ebaf1bfb47b096176a420e0eb90ca1618e1a07
lyniqna <fitriani.widodo18@gmail.com>
2025-11-17 00:29:30 +0700

    row

---
+++ Loop/jobsheet11/Square.java
@@ -0,0 +1,19 @@
+package Loop.jobsheet11;
+
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Enter the value of N: ");
+        int N = inp.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}

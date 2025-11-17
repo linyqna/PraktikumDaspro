0b51eed5103d46fa7e1bd31b0dd12d0c3eda39c6
lyniqna <fitriani.widodo18@gmail.com>
2025-11-17 20:17:29 +0700

    to discontinue

---
+++ Loop/jobsheet11/Quiz.java
@@ -0,0 +1,33 @@
+package Loop.jobsheet11;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Quiz {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        Random sub = new Random();
+
+        char menu = 'y';
+        do {
+            int number = sub.nextInt(10) + 1;
+            boolean success = false;
+            do {
+                System.out.print("Guess the number (1-10): ");
+                int answer = inp.nextInt();
+                inp.nextLine();
+                success = (answer == number);
+                
+                if (!success) {
+                    System.out.println("Wrong guess. Try again!");
+                } else {
+                    System.out.println("Correct! Well done.");
+                }
+
+            } while (!success);
+            System.out.println("Do you want to repeat the game (Y?N): ");
+            menu = inp.next().charAt(0);
+            inp.nextLine();
+        } while (menu == 'Y' || menu == 'y');
+    }
+}

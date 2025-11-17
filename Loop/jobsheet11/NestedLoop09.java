e55f96b6e268bd7ec9f23ddf2101d85915a23e49
lyniqna <fitriani.widodo18@gmail.com>
2025-11-17 20:56:11 +0700

    city temperature

---
+++ Loop/jobsheet11/NestedLoop09.java
@@ -0,0 +1,20 @@
+package Loop.jobsheet11;
+
+import java.util.Scanner;
+
+public class NestedLoop09 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        double[][] temps = new double[5][7];
+
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("City: " + i);
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print("Day " + (j + 1) + ": ");
+                temps[i][j] = inp.nextDouble();
+            }
+            System.out.println();
+        }
+    }
+}

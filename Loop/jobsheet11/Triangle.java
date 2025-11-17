5d43fdc07656e08b30d038d366c9e4323c57b70d
lyniqna <fitriani.widodo18@gmail.com>
2025-11-17 20:18:13 +0700

    final code

---
+++ Loop/jobsheet11/Triangle.java
@@ -0,0 +1,23 @@
+package Loop.jobsheet11;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Enter the value of N: ");
+        int N = inp.nextInt();
+
+        int i = 0;
+        while (i <= N) {
+            int j = 0;
+            while (j < i) {
+                System.out.print("*");
+                j++;
+            }
+            i++;
+            System.out.println("");
+        }
+    }
+}

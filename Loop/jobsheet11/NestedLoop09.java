67766df229573c2605f057e50ecb7f1239fc0011
lyniqna <fitriani.widodo18@gmail.com>
2025-11-17 21:19:55 +0700

    correct ver

--- Loop/jobsheet11/NestedLoop09.java
+++ Loop/jobsheet11/NestedLoop09.java
@@ -9,10 +9,18 @@ public class NestedLoop09 {
         double[][] temps = new double[5][7];
 
         for (int i = 0; i < temps.length; i++) {
-            System.out.println("City: " + i);
+            System.out.println("City: " + (i + 1));
             for (int j = 0; j < temps[0].length; j++) {
                 System.out.print("Day " + (j + 1) + ": ");
                 temps[i][j] = inp.nextDouble();
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("City: " + (i + 1));
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print(temps[i][j] + " ");
             }
             System.out.println();
         }

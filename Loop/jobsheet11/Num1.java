60b05a28b8d4f67ffe5aac5df683f0a3c58ccdd8
lyniqna <fitriani.widodo18@gmail.com>
2025-11-19 22:11:32 +0700

    done

---
+++ Loop/jobsheet11/Num1.java
@@ -0,0 +1,27 @@
+package Loop.jobsheet11;
+
+import java.util.Scanner;
+
+public class Num1 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Input N (min 3): ");
+        int N = inp.nextInt();
+
+        if (N < 3) {
+            System.out.println("N must be at least 3.");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}

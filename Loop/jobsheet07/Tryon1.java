661da256d3eb9b5bf164ad04564a57eeb37a3c6b
lyniqna <fitriani.widodo18@gmail.com>
2025-11-19 23:52:45 +0700

    new

---
+++ Loop/jobsheet07/Tryon1.java
@@ -0,0 +1,12 @@
+package Loop.jobsheet07;
+
+public class Tryon1 {
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

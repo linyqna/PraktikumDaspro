80a8dc117226c9adc21a22bd106738821c4824b3
lyniqna <fitriani.widodo18@gmail.com>
2025-11-20 13:54:49 +0700

    exp 2

---
+++ Array/Jobsheet9/Value09.java
@@ -0,0 +1,20 @@
+package Array.Jobsheet9;
+
+import java.util.Scanner;
+
+public class Value09 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int[] finalSc = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter the final score " + i + ": ");
+            finalSc[i] = inp.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Final score " + i + " is " + finalSc[i]);
+        }
+    }
+}

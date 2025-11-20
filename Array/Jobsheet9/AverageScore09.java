baaafb7b50077a0da8da460d35b40e2eca007444
lyniqna <fitriani.widodo18@gmail.com>
2025-11-20 19:53:45 +0700

    passed

--- Array/Jobsheet9/AverageScore09.java
+++ Array/Jobsheet9/AverageScore09.java
@@ -9,10 +9,14 @@ public class AverageScore09 {
         int[] score = new int[10];
         double total = 0;
         double average;
+        int passed = 0;
 
         for (int i = 0; i < score.length; i++) {
             System.out.print("Enter student score " + (i + 1) + ": ");
             score[i] = inp.nextInt();
+            if (score[i] > 70) {
+                passed++;
+            }
         }
 
         for (int i = 0; i < score.length; i++) {
@@ -21,5 +25,6 @@ public class AverageScore09 {
 
         average = total / score.length;
         System.out.println("The class average score is " + average);
+        System.out.println("The number of student Passed: " + passed);
     }
 }

995a223b19e5abd765ced6885e5db1ba66a603e5
lyniqna <fitriani.widodo18@gmail.com>
2025-11-20 15:35:02 +0700

    modify 2

--- Array/Jobsheet9/Number09.java
+++ Array/Jobsheet9/Number09.java
@@ -8,5 +8,9 @@ public class Number09 {
         System.out.println(num[1]);
         System.out.println(num[2]);
         System.out.println(num[3]);
+
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(num[i]);
+        }
     }
 }

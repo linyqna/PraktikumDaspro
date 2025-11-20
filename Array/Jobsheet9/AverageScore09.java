48bfb3be5353447640408ce64a44adf2136bfc40
lyniqna <fitriani.widodo18@gmail.com>
2025-11-20 14:14:56 +0700

    expr 3

---
+++ Array/Jobsheet9/AverageScore09.java
@@ -0,0 +1,25 @@
+package Array.Jobsheet9;
+
+import java.util.Scanner;
+
+public class AverageScore09 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int[] score = new int[10];
+        double total = 0;
+        double average;
+
+        for (int i = 0; i < score.length; i++) {
+            System.out.print("Enter student score " + (i + 1) + ": ");
+            score[i] = inp.nextInt();
+        }
+
+        for (int i = 0; i < score.length; i++) {
+            total += score[i];
+        }
+
+        average = total / score.length;
+        System.out.println("The class average score is " + average);
+    }
+}

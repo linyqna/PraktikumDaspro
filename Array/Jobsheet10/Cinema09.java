fed713d1941834eb2800c6a7f61b3152bcbd1222
lyniqna <fitriani.widodo18@gmail.com>
2025-11-23 22:36:43 +0700

    modify 4

--- Array/Jobsheet10/Cinema09.java
+++ Array/Jobsheet10/Cinema09.java
@@ -4,15 +4,19 @@ public class Cinema09 {
     public static void main(String[] args) {
         String audience[][] = new String[4][2];
 
+        System.out.println("Audiences in the row 3: ");
+        for (int i = 0; i < audience[2].length; i++) {
+            System.out.println(audience[2][i]);
+        }
+
         System.out.println(audience.length);
         for (int i = 0; i < audience.length; i++) {
             System.out.println("Length of row " + (i + 1) + ": " + audience[i].length);
         }
 
-        System.out.printf("%s \t %s \n", audience[0][0], audience[0][1]);
-        System.out.printf("%s \t %s \n", audience[1][0], audience[1][1]);
-        System.out.printf("%s \t %s \n", audience[2][0], audience[2][1]);
-        System.out.printf("%s \t %s \n", audience[3][0], audience[3][1]);
+        for (String[] rowAudience : audience) {
+            System.out.println("Length of row: " + rowAudience.length);
+        }
         
         System.out.println(audience.length);
         System.out.println(audience[0].length);

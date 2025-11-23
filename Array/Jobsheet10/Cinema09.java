a596942a396a624dab7bab40fd0d7b033f2c81a1
lyniqna <fitriani.widodo18@gmail.com>
2025-11-23 22:46:19 +0700

    modify 6

--- Array/Jobsheet10/Cinema09.java
+++ Array/Jobsheet10/Cinema09.java
@@ -5,8 +5,8 @@ public class Cinema09 {
         String audience[][] = new String[4][2];
 
         System.out.println("Audiences in the row 3: ");
-        for (String i : audience[2]) {
-            System.out.println(i);
+        for (int i = 0; i < audience.length; i++) {
+            System.out.println("Audience in the row " + (i + 1) + (": ") + String.join(", ", audience[i]));
         }
 
         System.out.println(audience.length);

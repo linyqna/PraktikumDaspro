25441fb4618647a4f7f94446153bf8283584b318
lyniqna <fitriani.widodo18@gmail.com>
2025-11-24 00:23:15 +0700

    modify 1

--- Array/Jobsheet10/Numbers09.java
+++ Array/Jobsheet10/Numbers09.java
@@ -1,5 +1,7 @@
 package Array.Jobsheet10;
 
+import java.util.Arrays;
+
 public class Numbers09 {
     public static void main(String[] args) {
         int[][] myNumbers = new int[3][];
@@ -7,5 +9,8 @@ public class Numbers09 {
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
         
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
     }
 }

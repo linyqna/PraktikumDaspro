62e183c81e255898b41328ba32e9ad6b78b0bc6e
lyniqna <fitriani.widodo18@gmail.com>
2025-12-02 21:31:30 +0700

    done

--- Quiz2.java
+++ Quiz2.java
@@ -23,8 +23,8 @@ public class Quiz2 {
         String[] month = {"Jan", "Feb", "March", "April"};
         int menu;
         double price = 1000000;
+        double total = 0;
         double discount = price * 0.05;
-        double priceFeb = price - discount;
 
         do {
             System.out.println("\n===Menu===");
@@ -38,21 +38,15 @@ public class Quiz2 {
             if (menu == 1) {
                 System.out.println("\n\t" + month[0] + "\t\t" + month[1] + "\t\t" + month[2] + "\t\t" + month[3]);
                 for (int i = 0; i < sales.length; i++) {
-                    System.out.println(employee[i][0] + "\t" + sales[i][0] * price + "\t" + sales[i][1] * priceFeb + "\t" + sales[i][2] * price + "\t" + sales[i][3] * price);
+                    total = sales[i].length * price;
+                    System.out.println(employee[i][0] + "\t" + sales[i][0] * price + "\t" + (sales[i][1] * (price - discount)) + "\t" + sales[i][2] * price + "\t" + sales[i][3] * price);
                 }
                 inp.nextLine();
             } else if (menu == 2) {
                 System.out.println("\n--Total Sales Per Month--");
-                for (int i = 0; i < month.length; i++) {
-                    double total = 0;
-                    for (int j = 0; j < sales.length; j++) {
-                        if (i == 1) {
-                            total += sales[j][1] * priceFeb;
-                        } else {
-                            total += sales[j][i] * price;
-                        }
-                    }
-                     System.out.println(month[i] + "\t: " + (int)total);
+                for (int i = 0; i < sales.length; i++) {
+                    total = (sales[i][0] * price) + (sales[i][1] * (price - discount)) + (sales[i][2] * price) + (sales[i][3] * price);
+                    System.out.println(month[i] + ": " + (int)total);
                 }
             } else if (menu == 3) {
                 System.out.println("Exiting program.");

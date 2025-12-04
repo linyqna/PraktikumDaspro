394f1ac0e6b3e9c2379e1194f2288aca9bc9ce82
lyniqna <fitriani.widodo18@gmail.com>
2025-12-04 15:25:12 +0700

    experiment 6

---
+++ function/Jobsheet13/Experiment609.java
@@ -0,0 +1,29 @@
+package function.Jobsheet13;
+
+import java.util.Scanner;
+
+public class Experiment609 {
+    static int calculateArea (int l, int w){
+        int area = l * w;
+        return area;
+    }
+    static int calculateVolume (int l, int w, int h){
+        int vol = calculateArea(l, w) * h;
+        return vol;
+    }
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int p, l, t, L, vol;
+        System.out.print("Input length = ");
+        p = inp.nextInt();
+        System.out.print("Input length = ");
+        l = inp.nextInt();
+        System.out.print("Input length = ");
+        t = inp.nextInt();
+
+        L = p * l;
+        System.out.println("Area of the rectangle = " + L);
+        vol = p * l * t;
+        System.out.println("Volume of the rectangle = " + vol);
+    }
+}

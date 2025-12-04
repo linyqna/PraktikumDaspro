607454191e5cc48fe0373ecab366546fddca8f3c
lyniqna <fitriani.widodo18@gmail.com>
2025-12-04 15:07:53 +0700

    update

---
+++ function/Jobsheet13/ExpressingGratitude09.java
@@ -0,0 +1,23 @@
+package function.Jobsheet13;
+
+import java.util.Scanner;
+
+public class ExpressingGratitude09 {
+    public static String getGreetingRecipient(){
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Input the name of people you want to greet: ");
+        String recipientName = inp.nextLine();
+        inp.close();
+        return recipientName;
+    }
+
+    public static void sayThankyou(){
+        String name = getGreetingRecipient();
+        System.out.println("Thank you " + name + " for being th ebest teacher in the world.\n" + 
+        "You inspired in me a love for learning and made me feel like I could ask you anything");
+    }
+
+    public static void main(String[] args) {
+        sayThankyou();
+    }
+}
---
+++ function/Jobsheet13/Greetings09.java
@@ -0,0 +1,18 @@
+package function.Jobsheet13;
+
+import java.util.Scanner;
+
+public class Greetings09 {
+    public static String getGreetingRecipient() {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Input the name of people you want to greet: ");
+        String recipientName = inp.nextLine();
+        inp.close();
+        return recipientName;
+    }
+
+    public static void main(String[] args) {
+        String name = getGreetingRecipient();
+        System.out.println("Thank you " + name + ", may the force be with you!");
+    }
+}
